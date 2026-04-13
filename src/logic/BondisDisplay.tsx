67981108620809351e0d08868f66b38798afceae
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-04-13 04:45:02 -0300

    fix: Mejorar funcion searchBus convirtiendo el array de lineas a un objeto para facilitar su acceso

--- src/logic/BondisDisplay.tsx
+++ src/logic/BondisDisplay.tsx
@@ -1,11 +1,21 @@
 import bondis from "../assets/bondis.json";
 import type { Bondi } from "../models/bondi";
 
-const listaBondis: Bondi[]  = bondis; 
+const arrBondis: Bondi[] = bondis; 
 
-const arrayToObjects = (list:number[]) : Bondi[] =>{
+const arrToObject = (list:Bondi[]): Record<number,Bondi> =>{
+    const objList = list.reduce <Record<number,Bondi>>((acc, bondi) => {
+        acc[bondi.linea] = bondi;
+        return acc
+    },{})
+    return objList
+}
+
+const objBondis: Record<number,Bondi> = arrToObject(arrBondis);
+
+const searchBus = (list:number[]) : Bondi[] =>{
     const arrList = list.map((n)=>{
-        const bondi = listaBondis.find(bondi => bondi.linea === n);
+        const bondi = objBondis[n];
         if(bondi){
             return bondi
         }
@@ -19,7 +29,7 @@ const arrayToObjects = (list:number[]) : Bondi[] =>{
 
 const BondisDisplay = ({list}:{list:number[]}) => {
     
-    const arrList : Bondi[] = arrayToObjects(list);
+    const arrList : Bondi[] = searchBus(list);
 
     return(
         <div>
