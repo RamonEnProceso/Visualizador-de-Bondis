efc1bd5920c4ff0a678c894bc4266364f4158509
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-04-13 04:20:11 -0300

    feat: Agregar componente de React que renderiza los carteles de bondis a partir de una lista de números

---
+++ src/logic/BondisDisplay.tsx
@@ -0,0 +1,39 @@
+import bondis from "../assets/bondis.json";
+import type { Bondi } from "../models/bondi";
+
+const listaBondis: Bondi[]  = bondis; 
+
+const arrayToObjects = (list:number[]) : Bondi[] =>{
+    const arrList = list.map((n)=>{
+        const bondi = listaBondis.find(bondi => bondi.linea === n);
+        if(bondi){
+            return bondi
+        }
+        return null
+    }).filter((b:Bondi|null): b is Bondi=>{
+        return  b !== null
+    })
+
+    return arrList
+}
+
+const BondisDisplay = ({list}:{list:number[]}) => {
+    
+    const arrList : Bondi[] = arrayToObjects(list);
+
+    return(
+        <div>
+            {arrList.map((b)=>{
+                return <div className="bondi-content" key={b.linea}>
+                <div className="bondi-line" style={{backgroundColor: b.firstLine}}></div>
+                <div className="bondi-line" style={{backgroundColor: b.lastLine}}></div>
+                <div className="bondi-num" style={{
+                    color: b.fontColor,
+                    backgroundColor: b.bkg
+                }}>{b.linea}</div></div>
+            })}
+        </div> 
+    )
+}
+
+export default BondisDisplay
